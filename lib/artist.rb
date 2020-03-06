9cc02dfc3e726fcca62c6f78216b9e5d715e69bd
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-06 13:40:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,6 +11,10 @@ def add_song(song)
   self.songs << song #Artist.songs reader? returns songs hopefully
 end
 
+def save
+  @@all << self #saves instances of Artist in @@all variable
+end
+
 def songs
   song.all.select{|song| song.artist = self}
 end
