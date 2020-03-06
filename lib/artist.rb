a9baebeb635291f063684d416af3d2b03676591c
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-06 13:46:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,4 +19,22 @@ end
     @@all
   end
 
+  def self.find_or_create_by_name(name) #find or create class meth to create instances?
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def self.find(name) #detects instancs from @@all class variable
+    self.all.detect {|artist| artist.name == name}
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
   end
