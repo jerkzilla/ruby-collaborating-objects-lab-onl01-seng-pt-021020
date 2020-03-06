ee2901ca56ab888c7b2eb352c8d90917048cebe6
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-06 13:29:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,13 @@
 class Song
-attr_accessor :artist, :name
-@@all = []
-def initialize(name)
-@name = name
-end
+  attr_accessor :artist, :name
+  @@all = []
+  def initialize(name)
+    @name = name
+  end
 
+  def self.all 
+    @@all 
+  end
+  
 
 end
